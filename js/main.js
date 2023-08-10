675ed0c8ff3ffa5513a963e3627f667b63d415eb
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2023-08-10 22:37:03 +0300

    add JS sript from question block

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-"use strict"
+"use strict";
+
+let faqItem = document.querySelectorAll(".faq-item");
+faqItem.forEach((faqItem) => {
+  faqItem.addEventListener("click", () => {
+    if (faqItem.classList.contains("faq-item")) {
+      faqItem.classList.toggle("faq-item__active");
+    }
+  });
+});
