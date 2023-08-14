a518c84812c7e41fe2a62cdda04f4aa6441911ec
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2023-08-14 19:31:46 +0300

    add dlider + fix marging menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,27 @@ faqItem.forEach((faqItem) => {
     }
   });
 });
+
+const sliderBtn = document.querySelectorAll(".slider-btn");
+let sliderLine = document.querySelector(".inform-block-line");
+let move = 0;
+
+sliderBtn.forEach((sliderBtn) => {
+  sliderBtn.addEventListener("click", () => {
+    if (sliderBtn.classList.contains("slider-btn__left")) {
+      move = move + 332;
+      if (move > 996) {
+        move = 0;
+      }
+      sliderLine.style.left = -move + "px";
+    } else {
+      if (move == 0) {
+        move = 996;
+        sliderLine.style.left = -move + "px";
+      } else {
+        move = move - 332;
+        sliderLine.style.left = -move + "px";
+      }
+    }
+  });
+});
